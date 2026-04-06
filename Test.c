f64a937d31915560bf9a830a7be206cc82432237
kovalevm2207 <kovalevm220741@gmail.com>
2026-04-06 03:36:04 +0300

    Поправил программу для тестов

--- Test.c
+++ Test.c
@@ -2,11 +2,31 @@
 
 extern int MyPrint(const char* const format);
 
+#define GREEN   "\033[32m"
+#define RED     "\033[31m"
+#define BLUE    "\033[34m"
+#define RESET   "\033[0m"
+
 int main()
 {
-    printf("(only string)\n\n");
-    int test1 = MyPrint("Mikle - the best engineer in the world");
-    printf("\ntest1 = %d\n\n", test1);
+    printf("Test number 1.\n"
+           "Test name: " BLUE "only format string\n" RESET
+           "\n"
+           "Test results:\n"
+           "MyPrint:    \n"
+           "            string: " GREEN);
+
+    int test = MyPrint("Mikle - the best engineer in the world");
+
+    printf(RESET "\n      return value: " GREEN "%d\n"
+                 "\n" RESET, test);
+    printf("printf:     \n"
+           "            string: " RED);
+
+    int ans = printf("Mikle - the best engineer in the world");
+
+    printf(RESET "\n      return value: " RED "%d\n" RESET, ans);
+
 
     return 0;
 }
