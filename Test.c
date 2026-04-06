59294a84975607ca9fe999f4a8d80d1e69fcf132
kovalevm2207 <kovalevm220741@gmail.com>
2026-04-06 19:40:38 +0300

    Выделил в отдельную функцию, функцию для теста MyPrint

--- Test.c
+++ Test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 extern int MyPrint(const char* const format);
 
@@ -7,26 +8,38 @@ extern int MyPrint(const char* const format);
 #define BLUE    "\033[34m"
 #define RESET   "\033[0m"
 
+void TestFunc(const char* const TestType, const char* const Format, const int TestNum);
+
 int main()
 {
-    printf("Test number 1.\n"
-           "Test name: " BLUE "only format string\n" RESET
+    TestFunc("clear format string",                       "Mike",                                 1);
+    TestFunc("clear format string and check drop buffer", "Mike- the best engineer in the world", 2);
+
+    return 0;
+}
+
+void TestFunc(const char* const TestType, const char* const Format, const int TestNum)
+{
+    assert(TestType); assert(Format);
+
+    printf("Test number %d.\n"
+           "Test name: " BLUE "%s\n" RESET
            "\n"
            "Test results:\n"
            "MyPrint:    \n"
-           "            string: " GREEN);
+           "            string: " GREEN, TestNum, TestType);
 
-    int test = MyPrint("Mikle - the best engineer in the world");
+    int test = MyPrint(Format);
 
     printf(RESET "\n      return value: " GREEN "%d\n"
                  "\n" RESET, test);
     printf("printf:     \n"
            "            string: " RED);
 
-    int ans = printf("Mikle - the best engineer in the world");
+    int ans = printf(Format);
 
-    printf(RESET "\n      return value: " RED "%d\n" RESET, ans);
+    printf(RESET "\n      return value: " RED "%d\n" RESET
+                 "--------------------------------------\n\n", ans);
 
-
-    return 0;
+    return;
 }
